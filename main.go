e53bf034c5a865f1c28ea5e85bcf393f9f973577
Mike Hewitt <matroxmike@icloud.com>
2020-06-02 09:21:21 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import ("fmt"
+  //      "encoding/json"
+ // see also dave cheny       )
+)
+func main() {
+//  a := make([]int,5)
+ // printSlice("a",a)
+ // var s * [] string
+x := []int {4,5,6}
+for i := range x {x = append(x,i) }
+fmt.Println(x)
+
+  fmt.Println("Hello World")
+}
+
+
+// printSlice func()
